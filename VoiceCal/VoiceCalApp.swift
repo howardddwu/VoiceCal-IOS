b578eb4e2907fb8789ec8a9f2c58064f078e8664
Howard Wu <howardddwu@gmail.com>
2024-11-19 15:48:57 -0500

    Initial Commit

---
+++ VoiceCal/VoiceCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceCalApp.swift
+//  VoiceCal
+//
+//  Created by Howard Wu on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct VoiceCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
